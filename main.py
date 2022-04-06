95541b39548c73803a3237599c81348696434529
colorchestra <git@morph.sh>
2022-04-06 21:35:42 +0200

    move config to separate file; add URL filtering

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import time
+import config
 
-index_url = ""
-webhook_url = ""
+index_url = config.index_url
+webhook_url = config.webhook_url
+filter_words = config.filter_words
+
 db_file = "db.txt"
 all_urls = []
 new_ads = 0
@@ -40,6 +43,14 @@ def notify(webhook_input):
     else:
         print("Payload delivered successfully, code {}.".format(result.status_code))
 
+def filter_url(input_url):
+    for word in filter_words:
+        if word in input_url:
+            print(f"{word} in {input_url}")
+            print(f"URL {input_url} contains a filtered word. You won't be notified.")
+            return False
+    return True
+
 def get_articles_from_index():
     index_result = requests.get(index_url, headers={'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0'})
     index_soup = BeautifulSoup(index_result.text, 'html.parser')
@@ -47,19 +58,19 @@ def get_articles_from_index():
         ad_url = article.get('data-href')
         ad_info = article.find('a', class_='ellipsis')
         ad_name = ad_info.string
-        print(ad_name)
         if ad_url not in all_urls:
             all_urls.append(ad_url)
+            if filter_url(ad_url):
+                print("New ad found! " + ad_name)
+                notify(webhook_input = f"Neue Wohnung!\n{ad_name}\nhttps://ebay-kleinanzeigen.de{ad_url}")
+                time.sleep(15)  # take a nap to avoid hitting Discord's rate limit. TODO: get the actual time from response headers
             # new_ads = new_ads + 1 # TODO
-            print("New ad found! " + ad_name)
-            notify(webhook_input = f"Neue Wohnung!\n{ad_name}\nhttps://ebay-kleinanzeigen.de{ad_url}")
-            time.sleep(15)  # take a nap to avoid hitting Discord's rate limit. TODO: get the actual time from response headers
 
 print("Reading database file...")
 read_file()
 print("Scraping ads from Ebay Kleinanzeigen...")
 get_articles_from_index()
-print("New ads found in this run: " + str(new_ads))
+#print("New ads found in this run: " + str(new_ads))
 print("Writing database...")
 write_file()
 print("Done.")
